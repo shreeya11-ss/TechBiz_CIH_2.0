6305164e01a21353a6f7f53ff2764cea1b81bdc6
Shreeya Sharma <shreeyasharma13112004@gmail.com>
2025-06-23 12:30:36 +0530

    Create : Vite App

---
+++ CrowdShield/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-4xl text-center text-white bg-blue-600 min-h-screen flex items-center justify-center">
+     CrowdShield 
+    </div>
+  );
+}
